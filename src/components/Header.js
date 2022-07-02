8e324d0cb2e4b0fd0e5ba9a5e383503a7cdb3d95
AzmatAli1 <mdhafizazmat@gmail.com>
2022-07-03 00:26:43 +0600

    login and sign up compleate

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import logo from '.././images/Logo.svg'
+import { Link } from 'react-router-dom';
 import './Header/Header.css';
 const Header = () => {
     return (
         <nav className='header'>
             <img src={logo} alt=""></img>
             <div>
-                <a href="/shop">Shop</a>
-                <a href="/orders">Orders</a>
-                <a href="/inventory">Inventory</a>
-                <a href="/about">About</a>
+                <Link to="/home">Home</Link>
+                <Link to="/shop">Shop</Link>
+                <Link to="/orders">Orders</Link>
+                <Link to="/about">About</Link>
+                <Link to="/inventory">Inventory</Link>
+                <Link to="/login">Login</Link>
             </div>
         </nav>
     );
---
+++ src/components/Require/Require.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Require = ({children}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Require;
